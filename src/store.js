d89e656c390fe9b7b6098eb14dff1179972e78cb
dida tulegenov <dida.tulegenov@fdmgroup.com>
2022-03-13 22:13:17 -0700

    f

--- src/store.js
+++ src/store.js
@@ -9,46 +9,25 @@ const middleware = [thunk];
 
 let store;
 
-// convert object to string and store in localStorage
-function saveToLocalStorage(state) {
-  try {
-    const serialisedState = JSON.stringify(state);
-    localStorage.setItem("persistantState", serialisedState);
-  } catch (e) {
-    console.warn(e);
-  }
-}
 
-// load string from localStarage and convert into an Object
-// invalid output must be undefined
-function loadFromLocalStorage() {
-  try {
-    const serialisedState = localStorage.getItem("persistantState");
-    if (serialisedState === null) return undefined;
-    return JSON.parse(serialisedState);
-  } catch (e) {
-    console.warn(e);
-    return undefined;
-  }
-}
 
-if (window.navigator.userAgent.includes("Chrome")) {
-  store = createStore(
-    rootReducer,
-    initalState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
-} else {
-  store = createStore(
-    rootReducer,
-    initalState,
-    compose(applyMiddleware(...middleware))
-  );
-}
+// if (window.navigator.userAgent.includes("Chrome")) {
+//   store = createStore(
+//     rootReducer,
+//     initalState,
+//     compose(
+//       applyMiddleware(...middleware),
+//       window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//       window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+//   );
+// } else {
+store = createStore(
+  rootReducer,
+  initalState,
+  compose(applyMiddleware(...middleware))
+);
+// }
 
 store.subscribe(() => {
   localStorage.setItem('fmovie', JSON.stringify(store.getState().fmovies))
